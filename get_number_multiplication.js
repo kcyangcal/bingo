8563bc6e6e25e4b33c3f2ed88b447664e2bd9eb2
Kai-Chuan Yang <kcyang@KCs-MacBook-Pro-14.local>
2023-09-10 01:10:54 -0700

    adding multiplication bingo

---
+++ get_number_multiplication.js
@@ -0,0 +1,62 @@
+$('#get_number').on('click', getNumber);
+$('#reset').on('click', reset_list);
+let generated_number_list = {};
+let generated_number_list_2 = [];
+
+function getNumber(){
+	var min_number = 1;
+	var max_number = 12;
+	if (Object.keys(generated_number_list).length < 144){
+		var [randomnumber, num1, num2] = generate_unique_multiplication(); // Updated to receive num1 and num2
+		let paragraph = document.getElementById("newnumber");
+		let num1Element = document.getElementById("num1"); // New Element
+		let num2Element = document.getElementById("num2"); // New Element
+		paragraph.innerHTML = randomnumber;
+		num1Element.innerHTML = "Num 1: " + num1; // New Element
+		num2Element.innerHTML = "Num 2: " + num2; // New Element
+		generated_number_list[randomnumber] = true;
+		generated_number_list_2.push(randomnumber);
+		update_list();
+		return true;
+	} else{
+		return false;
+	}
+}
+
+
+function check_exist_number(value, dictionary){
+	return (value in dictionary);
+}
+
+function generate_unique_multiplication(){
+	var min_number = 1;
+	var max_number = 12;
+	var need_one_ind = 1;
+	while(need_one_ind){
+		var num1 = Math.floor(Math.random() * (max_number - min_number + 1) + min_number);
+		var num2 = Math.floor(Math.random() * (max_number - min_number + 1) + min_number);
+		var multiplication_result = num1 * num2;
+		if(!check_exist_number(multiplication_result, generated_number_list)){
+			return [multiplication_result, num1, num2]; // Updated to return num1 and num2
+		}
+	}
+}
+
+
+function update_list(){
+	let paragraph_2 = document.getElementById("number_list");
+	let paragraph_3 = document.getElementById("number_list_2");
+	let existed_number = Object.keys(generated_number_list);
+	let existed_number_in_ordered = generated_number_list_2.join(',');
+	paragraph_2.innerHTML = "Here are the existed numbers in numerical order: " + existed_number;
+	paragraph_3.innerHTML = "Here are the numbers in the order of drawing: " + existed_number_in_ordered;
+}
+
+function reset_list(){
+	let paragraph_2 = document.getElementById("number_list");
+	let paragraph_3 = document.getElementById("number_list_2");
+	paragraph_2.innerHTML = " Click the Get number button to start the game! ";
+	paragraph_3.innerHTML = " ";
+	generated_number_list ={};
+	generated_number_list_2 =[];
+}
